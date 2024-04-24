c1a3bbd0c373ea93dab7f2729fb16084eb4cc141
Wojciech Bednarski <wojciech.bednarski.wb@gmail.com>
2024-04-24 18:17:20 +0200

    Fixed

---
+++ src/main/java/Kalkulator.java
@@ -0,0 +1,14 @@
+public class Kalkulator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        // You can add test code here to check if your methods work correctly
+        System.out.println("It works!");
+    }
+
+}
